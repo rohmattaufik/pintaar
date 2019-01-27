cf8e13385a18f6eea6d5a23386a3240693ce7026
Luqman hakim <mhd.luqmanhakim@gmail.com>
2019-01-27 18:20:28 +0700

    kelar

--- resources/views/layouts/course-order/purchase-success-with-share.blade.php
+++ resources/views/layouts/course-order/purchase-success-with-share.blade.php
@@ -14,10 +14,10 @@
           <div class="col-xs-12 col-md-8 col-md-offset-2 text-center">
               <div style="color:green;"><i class="fas fa-check-circle fa-7x"></i></div>
               <br>
-              <h4>Terima kasih sudah membeli kelas ini. Kelas ini GRATIS. Namun, kamu harus share postingan facebook Pintaar di timeline kamu dan kirimkan bukti screenshot ke fanpage Pintaar!</h4>
-              <h4>Jika sudah kamu share, kelas dapat kamu akses dalam 1x24 jam.</h4>
+              <h4>Terima kasih! Pembelian kelas kamu BERHASIL!</h4> 
+              <h4>Kelas ini GRATIS. Namun, kamu harus share postingan facebook Pintaar di timeline kamu. Jika sudah, kelas dapat kamu akses dalam 1x24 jam.</h4>
               <br>
-              <a href="https://www.facebook.com/pg/Pintaar-722335064826744/posts/?ref=page_internal" class="btn btn-lg btn-primary">Share Sekarang</a>
+              <a href="https://www.facebook.com/permalink.php?story_fbid=728135720913345&id=722335064826744" class="btn btn-lg btn-primary">Share Sekarang</a>
            
           </div>
         </div>      
