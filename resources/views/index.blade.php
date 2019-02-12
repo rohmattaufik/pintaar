465bb87cb68697614c82f0de9038bcdb0e70c498
Luqman hakim <mhd.luqmanhakim@gmail.com>
2019-02-12 09:27:09 +0700

    edit view harga diskon kelas

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -59,7 +59,7 @@
                                 @if($list_course_code->harga == 0)
                                   <h3 class="text-right"><span class="label label-warning">Gratis</span></h3>
                                 @else
-                                  <h4 class="text-right"><strike>Rp 50.000</strike> Rp {{ number_format($list_course_code->harga, 0, ',', '.') }}</h4>
+                                  <h4 class="text-right"><strike>Rp 100.000</strike> Rp {{ number_format($list_course_code->harga, 0, ',', '.') }}</h4>
                                 @endif
 
                              </div>
@@ -100,7 +100,7 @@
                                 @if($list_courses_others->harga == 0)
                                   <h3 class="text-right"><span class="label label-warning">Gratis</span></h3>
                                 @else
-                                  <h4 class="text-right">Rp {{ number_format($list_courses_others->harga, 0, ',', '.') }}</h4>
+                                  <h4 class="text-right"><strike>Rp 100.000</strike> Rp {{ number_format($list_courses_others->harga, 0, ',', '.') }}</h4>
                                 @endif
 
                              </div>
--- resources/views/layouts/course/index.blade.php
+++ resources/views/layouts/course/index.blade.php
@@ -6,78 +6,79 @@
 
 @section('content')
     <section class="section-padding">
-        <div class="container" style="word-wrap: break-word;">
-		  <h1> Ayo Belajar Sekarang!</h1>
-		  <br>
+        <div class="container">
           <h1>Kelas {{$kategori_kelas_str}}</h1>
           <br>
 
-          @foreach($list_courses_with_users as $list_course_with_user)
-               @if (($loop->index) % 4  == 0)
-                  <div class="row display-flex">
-                    <div class="col-xs-12 col-md-3">
-                      <a href="{{route(('course'), $list_course_with_user->id)}}">
-                         <div class="thumbnail">
-                             <img src="{{ URL::asset('images/gambar_course/'.$list_course_with_user->foto ) }}" alt="Gambar Kelas" height="120" width="500">
-                             <div class="caption">
-                                <h4>{{$list_course_with_user->nama_course}}</h4>
-                                <p><span class="ti-user"></span> {{$list_course_with_user->nama}}</p>
-                                <p class="starability-result" data-rating="{{ round($list_course_with_user->rating) }}"></p>
-                                @if($list_course_with_user->harga == 0)
-                                  <h3 class="text-right"><span class="label label-warning">Gratis</span></h3>
-                                @else
-                                  <h4 class="text-right">Rp {{ number_format($list_course_with_user->harga, 0, ',', '.') }}</h4>
-                                @endif
-                             </div>
-                         </div>
-                       </a>
+          @if (count($list_courses_with_users) == 0)
+            <h3>Kelas belum ada! Sabar yaaa...</h3>
+          @else
+            @foreach($list_courses_with_users as $list_course_with_user)
+                 @if (($loop->index) % 4  == 0)
+                    <div class="row display-flex">
+                      <div class="col-xs-12 col-md-3">
+                        <a href="{{route(('course'), $list_course_with_user->id)}}">
+                           <div class="thumbnail">
+                               <img src="{{ URL::asset('images/gambar_course/'.$list_course_with_user->foto ) }}" alt="Gambar Kelas" height="120" width="500">
+                               <div class="caption">
+                                  <h4>{{$list_course_with_user->nama_course}}</h4>
+                                  <p><span class="ti-user"></span> {{$list_course_with_user->nama}}</p>
+                                  <p class="starability-result" data-rating="{{ round($list_course_with_user->rating) }}"></p>
+                                  @if($list_course_with_user->harga == 0)
+                                    <h3 class="text-right"><span class="label label-warning">Gratis</span></h3>
+                                  @else
+                                    <h4 class="text-right"><strike>Rp 100.000</strike> Rp {{ number_format($list_course_with_user->harga, 0, ',', '.') }}</h4>
+                                  @endif
+                               </div>
+                           </div>
+                         </a>
+                      </div>
+                    
+                 @elseif (($loop->index) % 4  == 3)
+                      <div class="col-xs-12 col-md-3">
+                        <a href="{{route(('course'), $list_course_with_user->id)}}">
+                           <div class="thumbnail">
+                               <img src="{{ URL::asset('images/gambar_course/'.$list_course_with_user->foto ) }}" alt="Gambar Kelas" height="120" width="500">
+                               <div class="caption">
+                                  <h4>{{$list_course_with_user->nama_course}}</h4>
+                                  <p><span class="ti-user"></span> {{$list_course_with_user->nama}}</p>
+                                  <p class="starability-result" data-rating="{{ round($list_course_with_user->rating) }}"></p>
+                                  @if($list_course_with_user->harga == 0)
+                                    <h3 class="text-right"><span class="label label-warning">Gratis</span></h3>
+                                  @else
+                                    <h4 class="text-right"><strike>Rp 100.000</strike> Rp {{ number_format($list_course_with_user->harga, 0, ',', '.') }}</h4>
+                                  @endif
+            
+                               </div>
+                           </div>
+                         </a>
+                      </div>
                     </div>
-                  
-               @elseif (($loop->index) % 4  == 3)
-                    <div class="col-xs-12 col-md-3">
-                      <a href="{{route(('course'), $list_course_with_user->id)}}">
-                         <div class="thumbnail">
-                             <img src="{{ URL::asset('images/gambar_course/'.$list_course_with_user->foto ) }}" alt="Gambar Kelas" height="120" width="500">
-                             <div class="caption">
-                                <h4>{{$list_course_with_user->nama_course}}</h4>
-                                <p><span class="ti-user"></span> {{$list_course_with_user->nama}}</p>
-                                <p class="starability-result" data-rating="{{ round($list_course_with_user->rating) }}"></p>
-                                @if($list_course_with_user->harga == 0)
-                                  <h3 class="text-right"><span class="label label-warning">Gratis</span></h3>
-                                @else
-                                  <h4 class="text-right">Rp {{ number_format($list_course_with_user->harga, 0, ',', '.') }}</h4>
-                                @endif
-          
-                             </div>
-                         </div>
-                       </a>
-                    </div>
-                  </div>
 
-               @else
-                   <div class="col-xs-12 col-md-3">
-                      <a href="{{route(('course'), $list_course_with_user->id)}}">
-                         <div class="thumbnail">
-                             <img src="{{ URL::asset('images/gambar_course/'.$list_course_with_user->foto ) }}" alt="Gambar Kelas" height="120" width="500">
-                             <div class="caption">
-                                <h4>{{$list_course_with_user->nama_course}}</h4>
-                                <p><span class="ti-user"></span> {{$list_course_with_user->nama}}</p>
-                                <p class="starability-result" data-rating="{{ round($list_course_with_user->rating) }}"></p>
-                                
-                                @if($list_course_with_user->harga == 0)
-                                  <h3 class="text-right"><span class="label label-warning">Gratis</span></h3>
-                                @else
-                                  <h4 class="text-right">Rp {{ number_format($list_course_with_user->harga, 0, ',', '.') }}</h4>
-                                @endif
-                             </div>
-                         </div>
-                       </a>
-                   </div>
+                 @else
+                     <div class="col-xs-12 col-md-3">
+                        <a href="{{route(('course'), $list_course_with_user->id)}}">
+                           <div class="thumbnail">
+                               <img src="{{ URL::asset('images/gambar_course/'.$list_course_with_user->foto ) }}" alt="Gambar Kelas" height="120" width="500">
+                               <div class="caption">
+                                  <h4>{{$list_course_with_user->nama_course}}</h4>
+                                  <p><span class="ti-user"></span> {{$list_course_with_user->nama}}</p>
+                                  <p class="starability-result" data-rating="{{ round($list_course_with_user->rating) }}"></p>
+                                  
+                                  @if($list_course_with_user->harga == 0)
+                                    <h3 class="text-right"><span class="label label-warning">Gratis</span></h3>
+                                  @else
+                                    <h4 class="text-right"> <strike>Rp 100.000</strike> Rp {{ number_format($list_course_with_user->harga, 0, ',', '.') }}</h4>
+                                  @endif
+                               </div>
+                           </div>
+                         </a>
+                     </div>
 
-               @endif
+                 @endif
 
-              @endforeach
-
+                @endforeach
+             @endif
 
         </div>
     </section>
