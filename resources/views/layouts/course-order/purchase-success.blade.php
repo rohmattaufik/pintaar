9d98b4ea20489bf405d64b8da5894bfb3bac64d2
Luqman hakim <mhd.luqmanhakim@gmail.com>
2019-03-27 17:42:01 +0700

    tambah share ig story

--- resources/views/layouts/course-order/purchase-success.blade.php
+++ resources/views/layouts/course-order/purchase-success.blade.php
@@ -18,14 +18,16 @@
                 <div style="color:green;"><i class="fas fa-check-circle fa-7x"></i></div>
                 <br>
                 <h4>Terima kasih! Pembelian kelas kamu BERHASIL!</h4> 
-                <h4>Kelas ini GRATIS. Namun, kamu harus share postingan Pintaar di Line atau Facebook. Jika sudah, tunggu selama 1x24 jam dan kelas dapat kamu akses di menu <strong>Kelas Saya</strong>.</h4>
+                <p>Kelas ini GRATIS. Namun, kamu harus share satu postingan Pintaar di Story Instagram atau Line atau Facebook. Jika sudah, tunggu selama 1x24 jam dan kelas dapat kamu akses di menu <strong>Kelas Saya</strong>.</p>
                 <br>
                 <div class="row">
                   <div class="col-xs-12 col-md-6 col-md-offset-3">
-                      <a href="http://line.me/R/home/public/post?id=lov2313r&postId=1155273919305023787" target="_blank" class="btn btn-block btn-lg btn-success">Share di Line Sekarang</a>
+                      <a href="https://www.instagram.com/p/BvgiaIHgkcO/" target="_blank" class="btn btn-block btn-danger">Share di IG Story Sekarang</a>
+                      <p>atau</p>
+                      <a href="http://line.me/R/home/public/post?id=lov2313r&postId=1155273919305023787" target="_blank" class="btn btn-block btn-success">Share di Line Sekarang</a>
                       <p>atau</p>
                  
-                      <a href="https://www.facebook.com/permalink.php?story_fbid=728135720913345&id=722335064826744" target="_blank" class="btn btn-block btn-lg btn-primary">Share di Facebook Sekarang</a>
+                      <a href="https://www.facebook.com/permalink.php?story_fbid=728135720913345&id=722335064826744" target="_blank" class="btn btn-block btn-primary">Share di Facebook Sekarang</a>
                   </div> 
                 </div>     
 
