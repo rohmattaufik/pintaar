634845b351552aba5026469c52ed7afba412da33
Luqman hakim <mhd.luqmanhakim@gmail.com>
2019-04-10 19:50:33 +0700

    Revert "perbaikan fitur ubah profil pengajar"
    This reverts commit 2ca30f5031686072536c0e644a647152f3cd7bac.

--- database/migrations/2018_09_02_161828_create_tutors_table.php
+++ database/migrations/2018_09_02_161828_create_tutors_table.php
@@ -18,9 +18,8 @@ class CreateTutorsTable extends Migration
             $table->integer('id_user')->nullable();
             $table->string('name')->nullable();
             $table->string('profile_photo')->nullable();
-            $table->string('nama_bank')->nullable();
-            $table->string('no_rekening')->nullable();
-            $table->string('nama_rekening')->nullable();
+            $table->string('lama_mengajar')->nullable();
+            $table->string('pendidikan')->nullable();
             $table->longText('story')->nullable();
             $table->timestamps();
         });
